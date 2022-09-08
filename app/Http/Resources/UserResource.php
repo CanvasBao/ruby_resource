80e399ad79135f8ee2274cd4c6f6ae675ff7dc36
cnvs_bao <baongh.it.us@gmail.com>
2022-09-08 22:32:24 +0700

    add resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,15 +17,12 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->first_name . ' ' . $this->last_name,
-            'kana' => $this->first_name_kana . ' ' . $this->last_name_kana,
+            'company' => $this->company,
             'email' => $this->email,
-            'post_code' => $this->post_code,
-            'address_1' => $this->address_1,
-            'address_2' => $this->address_2,
+            'address' => $this->address,
             'tel' => $this->tel,
-            'birthday' =>  $this->birthday ? date('Y年m月d日', strtotime($this->birthday)) : '',
-            'created_at' => date('Y年m月d日 H:i:s', strtotime($this->created_at)),
-            'updated_at' => date('Y年m月d日 H:i:s', strtotime($this->updated_at))
+            'created_at' => date('Y/m/d H:i:s', strtotime($this->created_at)),
+            'updated_at' => date('Y/m/d H:i:s', strtotime($this->updated_at))
         ];
     }
 }
