38800ab7304706cc6e92a74dfd9115efcff39233
cnvs_bao <baongh.it.us@gmail.com>
2022-09-16 22:44:25 +0700

    fix lang

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.html')
 
 @section('meta')
-<x-shared.meta title="パスワードを忘れた方">
+<x-shared.meta title="Quên mật khẩu">
 </x-shared.meta>
 @endsection
 
@@ -9,7 +9,7 @@
 <div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
         <div>
-            <x-item.page-title title="パスワードを忘れた？" />
+            <x-item.page-title title="Quên mật khẩu？" />
             <p class="mt-2 text-center text-sm text-gray-600">
                 <a href="{{route('login')}}" class="font-medium text-indigo-600 hover:text-indigo-500">ログインへ </a>
             </p>
@@ -24,12 +24,12 @@
             {{ csrf_field() }}
             <div class="rounded-md shadow-sm -space-y-px">
                 <x-form.row>
-                    <x-form.input hideError="true" type="text" name="email" place="メールアドレス"/>
+                    <x-form.input hideError="true" type="text" name="email" place="e-mail"/>
                 </x-form.row>
             </div>
 
             <div>
-               <x-item.button>送信</x-item.button>
+               <x-item.button>Gửi</x-item.button>
             </div>
         </form>
     </div>
