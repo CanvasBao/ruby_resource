8e97f25668fc928c569f196b6b74686f0f197458
cnvs_bao <baongh.it.us@gmail.com>
2022-09-22 21:20:55 +0700

    add form item

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,16 +1,15 @@
 <div class="w-full {{ $boxclass ?? '' }}">
-    @php
-      $commonClass = "appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900
+  @php
+    $commonClass = "appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900
               rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm";
-    @endphp
-    <x-item.input {{ $attributes->except(['inpClass', 'class', 'hideError']) }}
-      class="{{ $commonClass }} {{ !empty($class) ? $class : '' }}">
-      {{ old("$name", $value ?? '') }}
-    </x-item.input>
-    @if (empty($hideError) || $hideError == false)
-      @error("$name")
-        <div class="my-1 font-medium text-[80%] leading-[120%] text-red-600 shadow-none">{{ $message }}</div>
-      @enderror
-    @endif
-  </div>
-  
+  @endphp
+  <x-item.input {{ $attributes->except(['inpClass', 'class', 'hideError']) }}
+    class="{{ $commonClass }} {{ !empty($class) ? $class : '' }}">
+    {{ old("$name", $value ?? '') }}
+  </x-item.input>
+  @if (empty($hideError) || $hideError == false)
+    @error("$name")
+      <div class="my-1 font-medium text-[80%] leading-[120%] text-red-600 shadow-none">{{ $message }}</div>
+    @enderror
+  @endif
+</div>
