acc04f4a4f8b119f149c8b04d642337623417cb1
cnvs_bao <baongh.it.us@gmail.com>
2022-09-23 21:29:37 +0700

    edit footer

--- resources/views/components/shared/footer.blade.php
+++ resources/views/components/shared/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-gray-50" aria-labelledby="footer-heading">
-    <h2 id="footer-heading" class="sr-only">Footer</h2>
-    <div class="max-w-md mx-auto pt-12 px-4 sm:max-w-7xl sm:px-6 lg:pt-16 lg:px-8">
-        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+  <h2 id="footer-heading" class="sr-only">Footer</h2>
+  <div class="max-w-md mx-auto pt-12 px-4 sm:max-w-7xl sm:px-6 lg:pt-16 lg:px-8">
+    {{-- <div class="xl:grid xl:grid-cols-3 xl:gap-8">
             <div class="space-y-8 xl:col-span-1">
                 <img class="h-10" src="https://tailwindui.com/img/logos/workflow-mark-gray-300.svg" alt="Company name">
                 <p class="text-gray-500 text-base">Making the world a better place through constructing elegant
@@ -138,9 +138,9 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="mt-12 border-t border-gray-200 py-8">
-            <p class="text-base text-gray-400 xl:text-center">&copy; 2020 Workflow, Inc. All rights reserved.</p>
-        </div>
+        </div> --}}
+    <div class="mt-12 border-t border-gray-200 py-8">
+      <p class="text-base text-gray-400 xl:text-center">created by BaoIT13</p>
     </div>
+  </div>
 </footer>
