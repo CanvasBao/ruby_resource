f9dd932fdc10f0d5020d1ae9682c94335f003761
cnvs_bao <baongh.it.us@gmail.com>
2022-09-24 11:41:41 +0700

    fix layout top

--- resources/views/pages/top.blade.php
+++ resources/views/pages/top.blade.php
@@ -1,14 +1,13 @@
 @extends('layout.html')
 
 @section('meta')
-<x-shared.meta title="change value like this!">
-    {{--
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"> --}}
-</x-shared.meta>
+  <x-shared.meta title="Trang chủ">
+    {{-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> --}}
+  </x-shared.meta>
 @endsection
 
 @section('content')
-<div class="min-h-screen flex items-center justify-center bg-slate-200 font-bold text-2xl">
+  <div class="min-h-screen flex items-center justify-center bg-slate-200 font-bold text-2xl">
     TOP PAGE
-</div>
+  </div>
 @endsection
