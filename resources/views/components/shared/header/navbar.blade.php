acb5a3aa168bb4579779ef0f1f68e9246455179c
cnvs_bao <baongh.it.us@gmail.com>
2022-09-25 12:33:29 +0700

    fix navbar

--- resources/views/components/shared/header/navbar.blade.php
+++ resources/views/components/shared/header/navbar.blade.php
@@ -1,6 +1,9 @@
 <x-shared.header.link name="top">
   Trang chủ
 </x-shared.header.link>
+<x-shared.header.link name="top">
+  Sản phẩm
+</x-shared.header.link>
 <x-shared.header.link name="contact.show">
   Liên hệ
 </x-shared.header.link>
