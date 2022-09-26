1862a1a376fcef333ef043d72ab64c6240651a58
cnvs_bao <baongh.it.us@gmail.com>
2022-09-26 20:19:47 +0700

    fix content

--- resources/views/components/shared/content-box.blade.php
+++ resources/views/components/shared/content-box.blade.php
@@ -1,5 +1,6 @@
-<div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 {{ $boxclass ?? '' }}">
-    <div class="w-full space-y-8 {{ $class ?? '' }}">
-        {{ $slot }}
-    </div>
+<div
+  class="min-h-fit flex items-center justify-center py-8 sm:py-14 lg:py-20 px-4 sm:px-6 lg:px-8 bg-slate-100 {{ $boxclass ?? '' }}">
+  <div class="w-full space-y-8 {{ $class ?? '' }}">
+    {{ $slot }}
+  </div>
 </div>
