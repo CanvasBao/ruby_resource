aa25bca335e53ff7abd2a23b24fea5b4dde52a7d
cnvs_bao <baongh.it.us@gmail.com>
2022-09-28 21:19:07 +0700

    add header

--- resources/views/components/shared/header/navbar.blade.php
+++ resources/views/components/shared/header/navbar.blade.php
@@ -1,7 +1,7 @@
 <x-shared.header.link name="top">
   Trang chủ
 </x-shared.header.link>
-<x-shared.header.link name="top">
+<x-shared.header.link name="product.show">
   Sản phẩm
 </x-shared.header.link>
 <x-shared.header.link name="contact.show">
