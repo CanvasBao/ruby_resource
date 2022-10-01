087bf58619a9ba2d944c698c508c3582441aa4e4
cnvs_bao <baongh.it.us@gmail.com>
2022-10-01 14:00:22 +0700

    edit component layout

---
+++ resources/views/components/layout/header/navbar.blade.php
@@ -0,0 +1,9 @@
+<x-shared.header.link name="top">
+  Trang chủ
+</x-shared.header.link>
+<x-shared.header.link name="product.show">
+  Sản phẩm
+</x-shared.header.link>
+<x-shared.header.link name="contact.show">
+  Liên hệ
+</x-shared.header.link>
