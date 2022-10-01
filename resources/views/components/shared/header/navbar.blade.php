8cc6250938e144d1aedd5ab4b18c814af0bfc87f
cnvs_bao <baongh.it.us@gmail.com>
2022-10-01 14:00:55 +0700

    delete

--- resources/views/components/shared/header/navbar.blade.php
+++
@@ -1,9 +0,0 @@
-<x-shared.header.link name="top">
-  Trang chủ
-</x-shared.header.link>
-<x-shared.header.link name="product.show">
-  Sản phẩm
-</x-shared.header.link>
-<x-shared.header.link name="contact.show">
-  Liên hệ
-</x-shared.header.link>
