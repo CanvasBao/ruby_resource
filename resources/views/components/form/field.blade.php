45c1949e8305d081fa1b945e52ff60dbaa17c41e
cnvs_bao <baongh.it.us@gmail.com>
2022-10-03 21:10:02 +0700

    edit field

--- resources/views/components/form/field.blade.php
+++ resources/views/components/form/field.blade.php
@@ -1,13 +1,18 @@
 <div class="{{ $boxclass ?? '' }}">
-  @if (!empty($title))
-    <label for="{{ $name }}" class="block text-sm font-medium text-gray-700">{{ $title ?? '' }}
-      @if (!empty($redtitle))
-        <span class="pl-1 text-[#9D0000] text-[80%]">{{ $redtitle ?? '' }}</span>
-      @endif
-    </label>
-  @endif
+  <div class="flex justify-between">
+    @if (!empty($title))
+      <label for="{{ $name }}" class="block text-sm font-medium text-gray-700">{{ $title ?? '' }}
+        @if (!empty($redtitle))
+          <span class="pl-1 text-[#9D0000] text-[80%]">{{ $redtitle ?? '' }}</span>
+        @endif
+      </label>
+    @endif
+    @if (!empty($note))
+      <span id="phone-optional" class="text-sm text-gray-500">{{ $note ?? '' }}</span>
+    @endif
+  </div>
   <div class="mt-1">
-    <x-item.input {{ $attributes->except(['boxclass', 'class', 'hideError', 'redtitle']) }}
+    <x-item.input {{ $attributes->except(['boxclass', 'class', 'hideError', 'redtitle', 'note']) }}
       class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm {{ $class ?? '' }}">
       {{ old("$name", $value ?? '') }}
     </x-item.input>
