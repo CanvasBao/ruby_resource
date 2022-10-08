edafdbeff90cf914a231abc479c7134bfa3b47b4
cnvs_bao <baongh.it.us@gmail.com>
2022-10-08 19:09:44 +0700

    fix product

--- resources/views/components/product/card.blade.php
+++ resources/views/components/product/card.blade.php
@@ -8,7 +8,7 @@
     {{-- <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
     <div class="h-full flex items-center justify-center">NO IMAGE</div>
   </div> --}}
-    <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-    <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
+    <h3 class="mt-4 text-sm text-gray-700">{{ $product->name }}</h3>
+    <p class="mt-1 text-lg font-medium text-gray-900">{{ $product->price }}</p>
   </a>
 @endisset
