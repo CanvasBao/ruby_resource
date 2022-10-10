013105e91afc509197390d6860d27f77e9c91442
cnvs_bao <baongh.it.us@gmail.com>
2022-10-10 21:11:55 +0700

    fix product db

--- database/migrations/2022_08_17_000002_create_product_image_table.php
+++ database/migrations/2022_08_17_000002_create_product_image_table.php
@@ -18,6 +18,7 @@ class CreateProductImageTable extends Migration
 
             $table->unsignedInteger('product_id');
             $table->string('image','100');
+            $table->longText('description')->nullable();
 
             $table->timestamp('created_at')->useCurrent();
 
