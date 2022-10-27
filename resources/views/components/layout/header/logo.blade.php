63dca6a1b4c4708a6962f1c3e3599434afebe204
cnvs_bao <baongh.it.us@gmail.com>
2022-10-27 21:45:07 +0700

    fix logo

--- resources/views/components/layout/header/logo.blade.php
+++ resources/views/components/layout/header/logo.blade.php
@@ -1,6 +1,7 @@
 <a href="{{ route('top') }}" class="flex-shrink-0 flex items-center">
-    <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+  {{-- <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
         alt="Workflow">
     <img class="hidden lg:block h-8 w-auto"
-        src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow">
+        src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow"> --}}
+  RUBY LABEL
 </a>
