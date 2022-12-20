d37ec6da61369bfc4d1d45924979f25466c327f5
cnvs_bao <baongh.it.us@gmail.com>
2022-12-20 22:43:02 +0700

    fix top slide

--- resources/views/components/top/slider.blade.php
+++ resources/views/components/top/slider.blade.php
@@ -1,29 +1,33 @@
 @isset($images)
-    <div class="swiper swiperTopPage">
-        <div class="swiper-wrapper">
-            @foreach ($images as $image)
-                <div class="swiper-slide ">
-                    <x-slider.card :item="$image" />
-                </div>
-            @endforeach
+  <div class="swiper swiperTopPage">
+    <div class="swiper-wrapper">
+      @foreach ($images as $image)
+        <div class="swiper-slide ">
+          <div class="w-full">
+            <figure class="w-full h-100">
+              <img class="w-full h-full object-cover" src="{{ $image }}" alt="slider image">
+            </figure>
+          </div>
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
+      @endforeach
     </div>
-    <script>
-        window.addEventListener("load", () => {
-            new Swiper('.swiperTopPage', {
-                loop: true,
-                navigation: {
-                    nextEl: ".swiper-button-next",
-                    prevEl: ".swiper-button-prev",
-                },
-                slidesPerView: "auto",
-                autoplay: {
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }
-            });
-        });
-    </script>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+  <script>
+    window.addEventListener("load", () => {
+      new Swiper('.swiperTopPage', {
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: "auto",
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        }
+      });
+    });
+  </script>
 @endisset
