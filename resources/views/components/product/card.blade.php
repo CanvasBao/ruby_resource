f0972c2c2294354384c9d3e3f7889bd0a073ef18
cnvs_bao <baongh.it.us@gmail.com>
2023-02-04 16:12:54 +0700

    edit product list

--- resources/views/components/product/card.blade.php
+++ resources/views/components/product/card.blade.php
@@ -1,17 +1,12 @@
 @isset($product)
-  <a href="{{ route('product.show', ['id' => $product->id, 'text' => urlencode($product->name)]) }}" class="group">
-    <div class="aspect-w-1 aspect-h-1 w-full shadow-md overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-      <img
-        src="{{ $product->images->count() > 0 ? asset('storage/uploads/product/' . $product->images[0]->image) : asset('storage/images/no_image.jpg') }}"
-        alt="{{ $product->name }}" class="h-full w-full object-cover object-center group-hover:opacity-75" />
+  <li>
+    <div class="space-y-4">
+      <div class="aspect-w-3 aspect-h-2">
+        <img class="rounded-lg object-cover shadow-lg"
+          src="{{ $product->images->count() > 0 ? asset('storage/uploads/product/' . $product->images[0]->image) : asset('storage/images/no_image.jpg') }}"
+          alt="">
+      </div>
+
     </div>
-    <h3 class="mt-4 text-sm text-gray-700">{{ $product->name }}</h3>
-    <p class="mt-1 text-lg font-medium text-gray-900">
-      @empty($product->price)
-        <span>Liên hệ</span>
-      @else
-        {{ number_format($product->price) }}<span class="text-[60%]"> VNĐ</span>
-      @endempty
-    </p>
-  </a>
+  </li>
 @endisset
--- resources/views/components/product/list.blade.php
+++ resources/views/components/product/list.blade.php
@@ -1,11 +1,13 @@
-<div class="bg-white">
-  <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h2 class="sr-only">Products</h2>
-
-    <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-      @foreach ($products as $product)
-        <x-product.card :product="$product" />
-      @endforeach
-    </div>
+<div class="space-y-12">
+  <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+    <h2 class="text-3xl font-bold tracking-tight sm:text-4xl">Our Products</h2>
+    <p class="text-xl text-gray-500">Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies
+      donec risus sodales. Tempus quis et.</p>
   </div>
+  <ul role="list"
+    class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
+    @foreach ($products as $product)
+      <x-product.card :product="$product" />
+    @endforeach
+  </ul>
 </div>
