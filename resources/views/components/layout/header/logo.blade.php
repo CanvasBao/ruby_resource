53effa033dbbc7ade5131e19c7e75aca8d0acef1
cnvs_bao <baongh.it.us@gmail.com>
2023-02-18 16:15:12 +0700

    edit logo

--- resources/views/components/layout/header/logo.blade.php
+++ resources/views/components/layout/header/logo.blade.php
@@ -1,7 +1,4 @@
 <a href="{{ route('top') }}" class="flex-shrink-0 flex items-center">
-  {{-- <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-        alt="Workflow">
-    <img class="hidden lg:block h-8 w-auto"
-        src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg" alt="Workflow"> --}}
-  RUBY LABEL
+  <img class="block h-8 w-auto" src="{{ asset('storage/images/logo.jpg') }}" alt="Workflow">
+  <label class="ml-2   text-xl font-bold text-red-600">RUBY LABEL</label>
 </a>
