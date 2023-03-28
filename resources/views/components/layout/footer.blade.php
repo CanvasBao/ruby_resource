683f6c0103de32b04893186e36f73ad90e827dc2
cnvs_bao <nguyenhabao@canvas-works.asia>
2023-03-28 13:56:16 +0700

    edit footer

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,8 +1,26 @@
 <footer class="bg-gray-50" aria-labelledby="footer-heading">
   <h2 id="footer-heading" class="sr-only">Footer</h2>
   <div class="max-w-md mx-auto pt-12 px-4 sm:max-w-7xl sm:px-6 lg:pt-16 lg:px-8">
-    <div class="border-t border-gray-200 py-8">
-      <p class="text-base text-gray-400 xl:text-center">created by BaoIT13</p>
+    <div class="md:grid md:grid-cols-2">
+      <div class="py-3">
+        <a href="{{ route('top') }}" class="">
+          <img class="block h-16 w-auto mx-auto" src="{{ asset('storage/images/logo.jpg') }}" alt="Workflow">
+          <label class="block text-center mx-auto mt-4 text-xl font-bold text-red-600">CÔNG TY TNHH RUBY LABEL</label>
+        </a>
+      </div>
+      <div class="py-3">
+        <h2 class="h2">THÔNG TIN LIÊN HỆ</h2>
+        Địa chỉ: Số 164/27B Bùi Quang Là, P12, Quận Gò Vấp, TP.Hồ Chí Minh, Việt Nam <br>
+        {{-- Địa chỉ nhà máy: Số 169/66 Nguyễn Hữu Cảnh, Phường Phú Thọ, Thành Phố Thủ Dầu Một, Tỉnh Bình Dương, Việt Nam. --}}
+
+        {{-- Email : info@binhduongbarcode.com - quocan@binhduongbarcode.com --}}
+      
+        Điện thoại : 028.220 363 79<br>
+        Fax: 028.220 363 78
+      </div>
+    </div>
+    <div class="border-t border-gray-200 py-2 mt-2">
+      <p class="text-xs text-gray-400 xl:text-center">@BIT</p>
     </div>
   </div>
 </footer>
