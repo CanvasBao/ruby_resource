02011255e293a9a59a7ec25189c5b9ba933e7aa1
baonghitus <baongh.it.us@gmail.com>
2023-12-05 17:29:50 +0700

    edit top page

--- resources/views/components/top/slider.blade.php
+++ resources/views/components/top/slider.blade.php
@@ -1,5 +1,5 @@
 @isset($images)
-  <div class="swiper swiperTopPage">
+  <div class="swiper swiperTopPage max-h-[640px] overflow-hidden">
     <div class="swiper-wrapper">
 
       <div class="swiper-slide" data-title=""  data-subtitle="">
